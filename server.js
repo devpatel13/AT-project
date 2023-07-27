1ab260f8d2359b98fd400b9a2cbd420a2fd72174
Devanshu Patel <87147446+devpatel13@users.noreply.github.com>
2023-07-27 09:32:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,16 +13,6 @@ app.use('/api/items' , itemsRoute);
 app.use('/api/users' , usersRoute);
 app.use('/api/bills' , billsRoute);
 
-const path = require('path')
-
-if(process.env.NODE_ENV==='production')
-{
-    app.use('/' , express.static('client/build'))
-    app.get('*' , (req,res)=>{
-         res.sendFile(path.resolve(__dirname , 'client/build/index.html'))
-    }) 
-}
-
 const port = process.env.PORT || 5000;
 
 app.get('/' , (req,res)=>res.send("Hello world!"))
